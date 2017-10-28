39c611a15d246a46ec419148e633f4e79643ea68
Shawn Taylor <staylor@cybraics.com>
2017-10-28 15:01:10 -0400

    Improve instagram map

--- instagram-map/map.js
+++ instagram-map/map.js
@@ -2,12 +2,8 @@ var map = L.map('map', {
   zoomControl: false
 }).setView([33.8, -84.4], 13);
 
-L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
-  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  subdomains: 'abcd',
-  minZoom: 0,
-  maxZoom: 18,
-  ext: 'png'
+L.tileLayer('https://api.mapbox.com/styles/v1/mapsandapps/cj9bj48ts4cza2rn2avra7xjt/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwc2FuZGFwcHMiLCJhIjoiY2o4YWpvYzJ5MGdpbDJxcDd0bzI5MDIwNiJ9.LimJsR1bhO-BQW80SlCiAQ', {
+  maxZoom: 18
 }).addTo(map);
 
 var icon = L.icon({
@@ -19,11 +15,10 @@ var icon = L.icon({
 var marker;
 
 var onMapClick = e => {
-  if (marker) { marker.removeFrom(map); }
+  if (marker) { map.removeLayer(marker); }
   marker = L.marker(e.latlng, { icon }).addTo(map);
+  map.panTo(e.latlng);
 }
-
-// TODO: make it draggable
 
 map.on('click', onMapClick);
 
