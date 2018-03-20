cc3e25797bbc04e5e46095919f2a3d84689e4770
Shawn Taylor <staylor@cybraics.com>
2018-03-19 21:25:44 -0400

    eBird recent sightings

---
+++ online-sources/ebird.js
@@ -0,0 +1,56 @@
+var hotspotID;
+var divContent = '';
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function constructLink(resp) {
+  divContent += `<a href="http://ebird.org/ebird/hotspot/${hotspotID}" target="_blank">See more recent observations from `;
+  if (resp.data && resp.data.length > 0) {
+    divContent += `${resp.data[0].locName} `;
+  } else {
+    divContent += 'this location ';
+  }
+  divContent += `on eBird</a>`;
+  document.getElementById("ebird").innerHTML = divContent;
+}
+
+function constructBody(resp) {
+  var noImpreciseBirds = [];
+  _.forEach(resp.data, function(value, key) {
+    if (!value.comName.includes(' sp.') && !value.comName.includes('/')) {
+      noImpreciseBirds.push(value.comName);
+    }
+  });
+  var sample = _.sampleSize(noImpreciseBirds, 10);
+  if(!sample || sample.length === 0) {
+    divContent += '<p>No recent observations</p>'
+  } else {
+    divContent += '<ul>';
+    _.forEach(sample, function(value) {
+      divContent += `<li>${value}</li>`;
+    });
+    divContent += '</ul>';
+  }
+}
+
+window.onload = function() {
+  hotspotID = getParameterByName('hotspot');
+  console.log(hotspotID);
+
+  axios.get(`https://ebird.org/ws1.1/data/obs/hotspot/recent?r=${hotspotID}&back=30&fmt=json&includeProvisional=true`)
+  .then(function (resp) {
+    constructBody(resp);
+    constructLink(resp);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
