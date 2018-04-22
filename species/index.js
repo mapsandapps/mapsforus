372bb49771e27b1aa0d893694c936dc1e9ca2d11
Shawn Taylor <staylor@cybraics.com>
2018-04-22 13:37:55 -0400

    Allow other albums to be displayed

--- species/index.js
+++ species/index.js
@@ -2,6 +2,15 @@ var API_KEY = '0f7476e73ab8d0c500a4b66c3067c82f';
 var SPECIES_PHOTOSET = '72157664978226738';
 var USER_ID = '149638594%40N05';
 
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+  function(m,key,value) {
+    vars[key] = value;
+  });
+  return vars;
+}
+
 function constructBody(resp) {
   var divContent = [];
   _.forEach(resp.data.photoset.photo, function(value, key) {
@@ -16,6 +25,12 @@ function constructBody(resp) {
 }
 
 window.onload = function() {
+  var albumID = getUrlVars()["album"];
+
+  if (albumID) {
+    SPECIES_PHOTOSET = albumID;
+  }
+
   axios.get(`https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&photoset_id=${SPECIES_PHOTOSET}&user_id=${USER_ID}&extras=&media=&format=json&nojsoncallback=1`)
   .then(function (resp) {
     constructBody(resp);
